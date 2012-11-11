87f1aba604763eae3f9761e70c0d6f6ca15b3780
amit-git <webprogrammer.amit@gmail.com>
2012-11-10 23:08:15 -0800

    reformat on save action

--- tst/org/skyblue/algorithms/FineUniqueTest.java
+++ tst/org/skyblue/algorithms/FineUniqueTest.java
@@ -6,11 +6,11 @@ import org.junit.Test;
 
 public class FineUniqueTest {
 
-	@Test
-	public void sampleTest() {
-		FindUnique fu = new FindUnique();
-        int [] input = { 1,1,1,1,2,2,3,3,3,3,4,4,4,5,5 };
+    @Test
+    public void sampleTest() {
+        FindUnique fu = new FindUnique();
+        int[] input = { 1, 1, 1, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5 };
         int k = fu.unique(input);
-		assertTrue(k == 4);
-	}
+        assertTrue(k == 4);
+    }
 }
